6d7e17b4b9a16cc198d0f88f0bc05c61d6661e2a
momizi06 <momizi06.th10@gmail.com>
2021-04-10 12:17:26 +0900

    documentation

--- docs/script.js
+++ docs/script.js
@@ -2,20 +2,20 @@
 console.log('hello!');
 
 (function($) {
-  var $nav   = $('#navArea');
-  var $btn   = $('.toggle_btn');
-  var $mask  = $('#mask');
-  var open   = 'open'; // class
-  // menu open close
-  $btn.on( 'click', function() {
-    if ( ! $nav.hasClass( open ) ) {
-      $nav.addClass( open );
-    } else {
-      $nav.removeClass( open );
-    }
-  });
-  // mask close
-  $mask.on('click', function() {
-    $nav.removeClass( open );
-  });
+    var $nav   = $('#navArea');
+    var $btn   = $('.toggle_btn');
+    var $mask  = $('#mask');
+    var open   = 'open'; // class
+    // menu open close
+    $btn.on( 'click', function() {
+        if ( ! $nav.hasClass( open ) ) {
+            $nav.addClass( open );
+        } else {
+            $nav.removeClass( open );
+        }
+    });
+    // mask close
+    $mask.on('click', function() {
+        $nav.removeClass( open );
+    });
 } )(jQuery);
